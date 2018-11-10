e5377dfbd128d34c8d5d8213adc679639f10e019
Anthony Demott <java152001@gmail.com>
2018-11-09 23:11:50 -0700

    Final Work

--- models/events.js
+++ models/events.js
@@ -1,10 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
   var Event = sequelize.define("Event", {
     eventName: DataTypes.STRING,
+    plannedBy: DataTypes.STRING,
     eventDate: DataTypes.DATE,
     eventTime: DataTypes.TIME,
     eventLocationName: DataTypes.STRING,
-    gameName: DataTypes.STRING
+    gameName: DataTypes.STRING,
+    attendees: DataTypes.STRING
   });
   return Event;
 };
--- routes/post-api-routes.js
+++ routes/post-api-routes.js
@@ -1,27 +1,5 @@
 var db = require('../models');
 
 module.exports = function (app) {
-  app.post("/event/add", function (req, res) {
-    db.Event.create(req.body).then(function (dbEvent) {
-      res.json(dbEvent);
-    });
-  });
-
-  app.get("/event/:search", function (req, res) {
-    db.Event.findAll({
-      where: {
-        gameName: req.params.search
-      }}).then(function (results) {
-        console.log(results);
-        if (results.length > 0) {
-          var hbsObject = {
-            events: results
-          }
-          res.render("index", hbsObject);
-        }
-        else {
-          res.render("404");
-        }
-      });
-    });
+  
 }
